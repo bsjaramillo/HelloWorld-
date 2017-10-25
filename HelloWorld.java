530db31fa5cb9f684b35eb63cad6994ec6e758da
bsjaramillo <brandonja1995@gmail.com>
2017-10-25 11:36:04 -0500

    Hello world with the chicken java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
-public class HelloWorld{
+public class HelloWorld {
 	public static void main (String[] args){
-		System.out.println ("Hello Brandon");
+		System.out.println ("Hola Mundo");
+	Chicken chicken;
+	chicken=new Chicken ();
+	chicken.initializeData("Lucy","Cafe",2);
+	chicken.print ();
 	}
 }
