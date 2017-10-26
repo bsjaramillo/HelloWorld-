1b7f03f2bb69ff5d003867a03058ceba40fca6ab
bsjaramillo <brandonja1995@gmail.com>
2017-10-25 20:09:44 -0500

    The work complete with the other chickens

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,26 @@ public class HelloWorld {
 		System.out.println ("Hola Mundo");
 	Chicken chicken;
 	chicken=new Chicken ();
-	chicken.initializeData("Lucy","Cafe",2);
+	chicken.initializeData("Brown",2);
+	chicken.addName("Lucy");
 	chicken.print ();
+	chicken.layAnEgg();
+	chicken.poop();
+	chicken.drink();
+	
+	Chicken ginger;
+	ginger=new Chicken();
+	ginger.addName("Ginger");
+	ginger.layAnEgg();
+	
+	Chicken anastasia;
+	anastasia=new Chicken();
+	anastasia.addName("Anastasia");
+	anastasia.drink();
+	
+	Chicken Mary;
+	Mary=new Chicken();
+	Mary.addName("Mary");
+	Mary.poop();
 	}
 }
