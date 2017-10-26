c29c0fe539669d514e9288221f35d362b86ff811
bsjaramillo <brandonja1995@gmail.com>
2017-10-25 20:08:35 -0500

    The chicken project with trhee more chickens

--- Chicken.java
+++ Chicken.java
@@ -1,23 +1,25 @@
 public class Chicken {
-		private String name;
+		private String data;
 		private String color;
 		private int age;
 		
 		public void layAnEgg(){
-			System.out.println ("The chicken lay an egg");
+			System.out.println ("The chicken "+data+" laid an egg\n");
 		}
 		public void poop(){
-			System.out.println ("The chicken did pooped");
+			System.out.println ("The chicken "+data+" did pooped\n");
 		}
 		public void drink(){
-			System.out.println ("The chicken drank");
+			System.out.println ("The chicken "+data+" drank water\n");
 		}
 		public void print(){
-			System.out.println ("The name of the chicken is:"+name+",color:"+color+",age:"+age+" ");
+			System.out.println ("The name of the chicken is:"+data+",color:"+color+",age:"+age+"\n ");
 		}
-		public void initializeData(String name,String color,int age){
-			this.name = name;
+		public void initializeData(String color,int age){
 			this.color = color;
 			this.age = age;
-		}	
+		}
+		public void addName(String data){
+			this.data = data;
+		}
 }
